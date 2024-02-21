5cdc87bed772e465517ddd7001f4861b5295d930
Simon Gariepy <116289143+S1m0n60@users.noreply.github.com>
2024-02-21 11:40:10 -0500

    Functions completed - waiting on implementation

--- class_moteurs.py
+++ class_moteurs.py
@@ -1,15 +1,18 @@
 from queue import Queue
 import RPi.GPIO as GPIO
 import time
+from math import radians
+from math import cos
+from math import sin
 
 class Moteurs:
     def __init__(self):
         """Initialisation des E/S pour le Raspberry Pi
         """
         # Init stepper
-        self.enable_pin1, self.coil_A1, self.coil_B1, self.coil_C1, self.coil_D1 = 1,23,20,22,12
-        self.enable_pin2, self.coil_A2, self.coil_B2, self.coil_C2, self.coil_D2 = 1,23,20,22,12
-        self.enable_pin3, self.coil_A3, self.coil_B3, self.coil_C3, self.coil_D3 = 11, 12, 13, 14, 15
+        self.enable_pin1, self.coil_A1, self.coil_B1, self.coil_C1, self.coil_D1 = 1,23,20,22,12 # Moteur laser
+        self.enable_pin2, self.coil_A2, self.coil_B2, self.coil_C2, self.coil_D2 = 1,23,20,22,12 # Moteurs plateau
+        self.enable_pin3, self.coil_A3, self.coil_B3, self.coil_C3, self.coil_D3 = 11, 12, 13, 14, 15 # Moteur verre
 
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
@@ -21,7 +24,10 @@ class Moteurs:
         GPIO.setup(self.limit_switch_pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         self.stepper_position_queue = Queue()
+        self.stepper_angle_queue = Queue()
         self.stepper_position = 0
+        self.stepper_angle = 0
+        self.angle_rotation = 10
 
     def is_limit_switch_triggered(self, switch_id):
         """Fonction pour vérifier si un des capteurs de fin de course est activé
@@ -35,42 +41,25 @@ class Moteurs:
         """
         return GPIO.input(self.limit_switch_pins[switch_id-1])
     
-    def enable_stepper_motor(self, motor_id):
-        """Fonction permettant d'activer un des moteurs pas-à-pas
+    def stop_motors(self):
+        """Fonction permettant d'arrêter les moteurs"""
+        # Turn off all coils for motor 1
+        GPIO.output(self.coil_A1, GPIO.LOW)
+        GPIO.output(self.coil_B1, GPIO.LOW)
+        GPIO.output(self.coil_C1, GPIO.LOW)
+        GPIO.output(self.coil_D1, GPIO.LOW)
 
-        Args:
-            motor_id (int): Identifiant du moteur pas-à-pas (1-3)
-        """
-        enable_pin = self.get_enable_pin(motor_id)
-        GPIO.output(enable_pin, GPIO.HIGH)
+        # Turn off all coils for motor 2
+        GPIO.output(self.coil_A2, GPIO.LOW)
+        GPIO.output(self.coil_B2, GPIO.LOW)
+        GPIO.output(self.coil_C2, GPIO.LOW)
+        GPIO.output(self.coil_D2, GPIO.LOW)
 
-    def disable_stepper_motor(self, motor_id):
-        """Fonction permettant de désactiver un des moteurs pas-à-pas
-
-        Args:
-            motor_id (int): Identifiant du moteur pas-à-pas (1-3)
-        """
-        enable_pin = self.get_enable_pin(motor_id)
-        GPIO.output(enable_pin, GPIO.LOW)
-
-    def get_enable_pin(self, motor_id):
-        """Fonction permettant de retourner la pin d'activation du moteur concerné
-
-        Args:
-            motor_id (int): Identifiant du moteur pas-à-pas (1-3)
-
-        Returns:
-            int: Valeur de la pin correspondante à l'identifiant du moteur en args
-        """
-        if motor_id == 1:
-            return self.enable_pin1
-        elif motor_id == 2:
-            return self.enable_pin2
-        elif motor_id == 3:
-            return self.enable_pin3
-        else:
-            print(f"Invalid motor_id: {motor_id}")
-            return None
+        # Turn off all coils for motor 3
+        GPIO.output(self.coil_A3, GPIO.LOW)
+        GPIO.output(self.coil_B3, GPIO.LOW)
+        GPIO.output(self.coil_C3, GPIO.LOW)
+        GPIO.output(self.coil_D3, GPIO.LOW)
 
     def move_stepper_motor_forward(self, motor_id, steps, speed):
         """Fonction permettant de faire avancer un moteur pas-à-pas selon une vitesse et un nombre de pas spécifié
@@ -193,100 +182,112 @@ class Moteurs:
         """Fonction permettant le déplacement du moteur pas-à-pas jusqu'à l'activation d'un des 2 capteurs de fin de course
         """
         motor_id = 1 
-        self.enable_stepper_motor(motor_id)
            
-        self.move_stepper_motor_forward(motor_id, steps=1000, speed=600)
+        self.move_stepper_motor_forward(motor_id, steps=1000, speed=450)
 
         self.stepper_position = 0
-        self.disable_stepper_motor(motor_id)
 
     def move_board_up(self):
         """Fonction permettant de bouger les moteurs 2 et 3 pas-à-pas en même temps pour faire bouger la plateforme vers le haut
         """
         motor_id = 2
-        self.enable_stepper_motor(motor_id)
 
-        self.move_stepper_motor_forward(motor_id, steps=10000, speed=600)
-
-        self.disable_stepper_motor(motor_id)
+        self.move_stepper_motor_forward(motor_id, steps=10000, speed=450)
 
     def move_board_down(self):
         """Fonction permettant de bouger les moteurs 2 et 3 pas-à-pas en même temps pour faire bouger la plateforme vers le bas
         """
-        limit_switch_id1 = 3
-        limit_switch_id2 = 4
-
-        limit_switch_pin1 = self.limit_switch_pins[limit_switch_id1 - 1]
-        limit_switch_pin2 = self.limit_switch_pins[limit_switch_id2 - 1]
-
         motor_id = 2
-        self.enable_stepper_motor(motor_id)
 
+        self.move_stepper_motor_backwards(motor_id, steps=10000, speed=450)
 
-        while (self.is_limit_switch_triggered(limit_switch_pin1) == 0 or self.is_limit_switch_triggered(limit_switch_pin2) == 0):
-            self.move_stepper_motor_backwards(motor_id, steps=1, speed=10)
-
-        self.disable_stepper_motor(motor_id)
-    
-    def gravure(self, queue_longueur_verre, queue_distance_debut_gravure, queue_angle_rotation_intermediaire, queue_marque_fin_gravure):
-        """Fonction permettant de réaliser la gravure sur le verre.
+    def move_board_to_pos(self, queue_radius):
+        """Fonction permettant de bouger le plateau a la position de depart
 
         Args:
-            queue_longueur_verre (Queue): File d'attente pour la longueur du verre en mm.
-            queue_distance_debut_gravure (Queue): File d'attente pour la distance du début de la gravure à partir du centre du verre en mm.
-            queue_angle_rotation_intermediaire (Queue): File d'attente pour l'angle de rotation intermédiaire en degrés.
-            queue_marque_fin_gravure (Queue): File d'attente pour la variable indiquant la fin de la gravure.
-            queue_positions (Queue): File d'attente pour stocker les positions du moteur.
+            queue_radius (float): Rayon récupéré depuis la file d'attente.
+        """
+
+        motor_id = 2
+        diametre_verre = 2 * queue_radius.get()
+        hauteur = 200
+        distance_focale = 10
+        position = hauteur - distance_focale - diametre_verre
+
+        self.move_stepper_to_distance(motor_id, position, 450)
+
+    
+    def gravure(self, queue_longueur_verre, queue_distance_debut_gravure, queue_button_start, queue_radius):
+        """Fonction de séquence de gravure du verre
+
+        Args:
+            queue_longueur_verre (float): Longueur du verre récupérée depuis la file d'attente.
+            queue_distance_debut_gravure (float): Distance de début de la gravure récupérée depuis la file d'attente.
+            queue_button_start (bool): État du bouton de démarrage récupéré depuis la file d'attente.
+            queue_radius (float): Rayon récupéré depuis la file d'attente.
         """
         # Récupération des paramètres depuis les queues
         longueur_verre = queue_longueur_verre.get()
         distance_debut_gravure = queue_distance_debut_gravure.get()
-        angle_rotation_intermediaire = queue_angle_rotation_intermediaire.get()
-        marque_fin_gravure = queue_marque_fin_gravure.get()
+        angle_rotation_intermediaire = self.angle_rotation
 
-        # Position initiale du moteur 1
+        # Initial position of motor 1
         longueur_totale = 30
-        position_initiale = (longueur_totale/2 + longueur_verre / 2 + distance_debut_gravure)
+        position_initiale = (longueur_totale / 2 + longueur_verre / 2 + distance_debut_gravure)
 
-        # Position du moteur 1 pour débuter la gravure
+        # Position of motor 1 to start engraving
         self.move_stepper_to_distance(motor_id=1, distance=position_initiale, speed=600)
 
-        while not marque_fin_gravure:
-            # Aller
-            self.move_stepper_to_distance(motor_id=1, distance=longueur_verre, speed=600)
-            self.stepper_position_queue.put((1, longueur_verre))
-            # Retour
-            self.move_stepper_to_distance(motor_id=1, distance=-longueur_verre, speed=600)
-            self.stepper_position_queue.put((1, -longueur_verre))
-            # Rotation moteur 3
-            self.move_stepper_motor_forward(motor_id=3, steps=int(angle_rotation_intermediaire), speed=600)
-            self.angle_position_queue.put(angle_rotation_intermediaire)
+        radius = queue_radius.get() 
+        final_position_theta = radians(angle_rotation_intermediaire)
+        final_position_x = radius * cos(final_position_theta)
+        final_position_y = radius * sin(final_position_theta)
 
-        print("Gravure terminée.")
-       
+        # Check for button press to start engraving
+        if not queue_button_start.empty():
+            button_press = queue_button_start.get()
+            if button_press == "debut*":
+                # Move forward
+                self.move_stepper_to_distance(motor_id=1, distance=longueur_verre, speed=600)
+                self.stepper_position_queue.put((1, longueur_verre))
+                # Move backward
+                self.move_stepper_to_distance(motor_id=1, distance=-longueur_verre, speed=600)
+                self.stepper_position_queue.put((1, -longueur_verre))
+                # Rotate motor 3
+                self.move_stepper_motor_forward(motor_id=3, steps=int(angle_rotation_intermediaire), speed=600)
+                self.angle_position_queue.put(angle_rotation_intermediaire)
+                current_position_x, current_position_y = self.read_stepper_position()
+
+                if current_position_x >= final_position_x and current_position_y <= final_position_y:
+                    self.stop_motors()
+
+
     def read_stepper_position(self):
-        """Fonction permettant de retourner les valeurs de positions parcourues en temps réel par le moteur 2 et la position d'angle du moteur 3.
-
-        Returns:
-            tuple: (Position moteur 2, Position d'angle moteur 3)
-        """
+        """Fonction permettant de mettre les valeurs de positions parcourues en temps réel par le moteur 2 et la position d'angle du moteur 3 dans les files d'attente."""
         stepper_position = 0
         angle_position = 0
 
-        # Parcourir les éléments de la file d'attente pour obtenir les positions en temps réel
         while not self.stepper_position_queue.empty():
             motor_id, steps = self.stepper_position_queue.get()
             if motor_id == 2:
                 stepper_position += steps
-        while not self.angle_position_queue.empty():
-            angle_position = self.angle_position_queue.get()
+            elif motor_id == 3:
+                angle_position += steps
+    
+            self.stepper_position_queue.put((motor_id, steps))
+            self.stepper_angle_queue.put((motor_id, steps))
 
-        return stepper_position, angle_position
+    
+    def sequence(self, queue_longueur_verre, queue_distance_debut_gravure, queue_button_start, queue_radius):
+        self.laser_go_to_home()
+        self.move_board_down()
+        self.move_board_to_pos(queue_radius)
+        self.gravure(queue_longueur_verre, queue_distance_debut_gravure, queue_button_start, queue_radius)
+        self.laser_go_to_home()
+        self.move_board_down()
 
 
 testmoteur = Moteurs()
-#testmoteur.enable_stepper_motor(1)
-#testmoteur.enable_stepper_motor(4)
 #testmoteur.move_stepper_motor_forward(4,1,10)
 #testmoteur.move_stepper_motor_backwards(1,800,200)
 #testmoteur.move_stepper_motor_forward(1,200,200)
