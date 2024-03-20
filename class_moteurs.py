3bf9421b6189f8129c9768173c891c9ec4c194c0
floP1511 <flo.paquettehotmail.ca>
2024-03-20 16:19:04 -0400

    added zero

--- class_moteurs.py
+++ class_moteurs.py
@@ -341,12 +341,12 @@ class Moteurs:
         position_initiale = 30
 
         self.move_stepper_to_distance(motor_id=1, distance=position_initiale, speed=450)
-        
+        self.stepper_position[0] = 0
         button_press = self.queue_button_start
         if button_press:
             sens = 1
             while self.stepper_position[2]*(pi*self.queue_radius/100) < self.queue_gravx:
-                self.move_stepper_to_distance(motor_id=1, distance=longueur_totale*sens, speed=200)
+                self.move_stepper_to_distance(motor_id=1, distance=longueur_totale*sens, speed=450)
                 self.move_stepper_motor_forward(motor_id=3, steps=5, speed=350)
                 sens *= -1
                 time.sleep(0.25)
