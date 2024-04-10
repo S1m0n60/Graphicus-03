755a4086c3b3f7a890df6dc54d1abf0c2e3a38a8
S1m0n60 <simongariepy60@gmail.com>
2024-04-10 09:41:12 -0400

    demo_grav test

--- demo_grav.py
+++ demo_grav.py
@@ -18,11 +18,12 @@ def motor1(moteurs):
 def motor3(moteurs):
 
     while True:
-        moteurs.move_stepper_motor_forward(3,500,75)
-        moteurs.move_stepper_motor_forward(3,500,350)
-        moteurs.move_stepper_motor_backwards(3,500,75)
-        moteurs.move_stepper_motor_backwards(3,500,250)
-        time.sleep(0.05)
+        moteurs.move_stepper_motor_forward(3,4,300)
+        # moteurs.move_stepper_motor_forward(3,500,350)
+        # moteurs.move_stepper_motor_backwards(3,500,75)
+        time.sleep(1)
+        moteurs.move_stepper_motor_backwards(3,4,300)
+        time.sleep(1)
 
 #-----------Creation de thread----------------
 motor1Thread = Thread(target=motor1, args=[moteurs])
