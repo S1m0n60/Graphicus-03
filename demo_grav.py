5184194c10fc04891a4e9862b5f24c32964a0251
S1m0n60 <simongariepy60@gmail.com>
2024-04-10 10:33:15 -0400

    changed demo grav

--- demo_grav.py
+++ demo_grav.py
@@ -12,7 +12,7 @@ def motor1(moteurs):
         time.sleep(1)
         moteurs.move_stepper_motor_forward(1,10000,450)
         time.sleep(1)
-        moteurs.move_stepper_motor_backwards(1,1000,450)
+        moteurs.move_stepper_motor_backwards(1,10000,450)
         time.sleep(1)
 
 def motor3(moteurs):
